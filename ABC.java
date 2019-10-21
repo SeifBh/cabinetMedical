2606d01c63b4901236f1aba2be8d865aeaad3029
SEIF BH HEROKU <belhadjali.seif@gmail.com>
2019-10-21 09:45:36 +0100

    ABC-ABC

---
+++ ABC.java
@@ -0,0 +1,25 @@
+
+public class ABC{
+
+	public void A(){
+
+		System.Out.println("Bonjour version ABC-A");
+
+	}
+
+
+		public void B(){
+
+		System.Out.println("Bonjour version ABC-B");
+		
+	}
+
+
+		public void C(){
+
+		System.Out.println("Bonjour version ABC-C");
+		
+	}
+
+
+}
